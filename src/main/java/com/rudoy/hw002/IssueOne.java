4f31437ad6e9f3b77f9f4a2c68fb012b93bd69d4
Andrey <andrey.rudy1@gmail.com>
2017-02-26 15:31:36 +0200

    My first home-work

--- src/main/java/com/rudoy/hw002/IssueOne.java
+++ src/main/java/com/rudoy/hw002/IssueOne.java
@@ -4,7 +4,15 @@ package com.rudoy.hw002;
  * Created by OmEz on 25.02.2017.
  */
 public class IssueOne {
+    public static double toKelvin(double celsius){
+        return celsius - 273.15;
+    }
+    public static double toFahrenheit(double celsius){
+        return  celsius * 1.8 + 32.0;
+    }
     public static void main (String[] args) {
-
+        double temperature = 36.6;
+        System.out.println("Kelvin: " + toKelvin(temperature));
+        System.out.println("Fahrenheit: " + toFahrenheit(temperature));
     }
 }
