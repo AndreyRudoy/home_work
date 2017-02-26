e0557cc43aaf5a4267bc0a2c9df4151ef66f5dd1
Andrey <andrey.rudy1@gmail.com>
2017-02-26 15:50:10 +0200

    hm2.2

--- src/main/java/com/rudoy/hw002/IssueTwo.java
+++ src/main/java/com/rudoy/hw002/IssueTwo.java
@@ -1,8 +1,22 @@
 package com.rudoy.hw002;
 
+import java.util.Scanner;
+
 /**
  * Created by OmEz on 26.02.2017.
  */
 public class IssueTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) { // возвращает истинну если с потока ввода можно считать целое число
+            int number = sc.nextInt(); // считывает целое число с потока ввода и сохраняем в переменную
+            int low = number % 10;
+            int up = number / 10;
+            System.out.println(low + "" + up);
+        } else {
+            System.out.println("Вы ввели не целое число");
+        }
+
+    }
 
 }
