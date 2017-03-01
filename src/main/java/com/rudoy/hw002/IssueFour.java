e7aad446866aef14cd1cb3f3d6020c6e852ce5f6
Andrey <andrey.rudy1@gmail.com>
2017-03-01 15:57:36 +0200

    hm2.4

--- src/main/java/com/rudoy/hw002/IssueFour.java
+++ src/main/java/com/rudoy/hw002/IssueFour.java
@@ -4,7 +4,20 @@ package com.rudoy.hw002;
  * Created by OmEz on 26.02.2017.
  */
 public class IssueFour {
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        boolean x, y;
+        x = true;
+        y = false;
+        //а) не (X и не Y) или X;
+        //б) Y и не X или не Y;
+        //в) не Y и не X или Y.
+        boolean a = !(x & (! y)) | x;
+        boolean b = y & (! x) | (! y);
+        boolean c = ! y & (! x) | y;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
 
     }
 }
