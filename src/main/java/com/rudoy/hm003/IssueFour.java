b0e92b6a4d68b3aa59a0279c318a042c56e94ab6
Andrey <andrey.rudy1@gmail.com>
2017-03-05 16:18:11 +0200

    hm3.2

---
+++ src/main/java/com/rudoy/hm003/IssueFour.java
@@ -0,0 +1,15 @@
+package com.rudoy.hm003;
+
+/**
+ * Created by OmEz on 05.03.2017.
+ */
+public class IssueFour {
+    public static void main (String[] args){
+//Вывести последовательность 7, 9, 11 пока число меньше 100.
+        int i = 7;
+        while(i<100){
+            System.out.println(i);
+            i = i + 2;
+        }
+    }
+}
