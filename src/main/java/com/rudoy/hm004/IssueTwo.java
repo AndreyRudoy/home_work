5cda837b013028b3124d45d5c46d04f2564243c2
Andrey <andrey.rudy1@gmail.com>
2017-03-12 21:27:49 +0200

    hm4.3

---
+++ src/main/java/com/rudoy/hm004/IssueTwo.java
@@ -0,0 +1,25 @@
+package com.rudoy.hm004;
+
+/**
+ * Created by OmEz on 11.03.2017.
+ */
+public class IssueTwo {
+
+    public long factorial(int n) {
+        long product = 1;
+        if (n > 0) {
+            for (int i = 1; i < n + 1; i++) {
+                product = product * i;
+            }
+        }
+        return product;
+    }
+
+    public static void main(String[] args) {
+        double x = 5;
+        for (int i = 0; i < x; i++) {
+            double a = 0;
+            //for (int j = 0; j < i; j++)
+        }
+    }
+}
