24514d17e9bd4183417f1a840b4e52de789e24b8
Andrey <andrey.rudy1@gmail.com>
2017-03-30 18:35:14 +0300

    hm10.2

--- src/main/java/com/rudoy/hm010/Issue2.java
+++ src/main/java/com/rudoy/hm010/Issue2.java
@@ -1,14 +1,15 @@
 package com.rudoy.hm010;
 
-import java.util.Random;
-
 /**
  * Created by OmEz on 30.03.2017.
  */
 public class Issue2 {
+
+    public static boolean div(float n) {
+        return n % 2 == 0;
+    }
+
     public static void main(String[] args) {
-
-
-
+        System.out.println(div(35.7f));
     }
 }
