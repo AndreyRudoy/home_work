bb0efe320d2520fca13ce758b8eb832c04daa2bb
Andrey <andrey.rudy1@gmail.com>
2017-05-20 21:53:19 +0300

    hm18.8

--- src/main/java/com/rudoy/hm018/Issue6/Neurosurgeon.java
+++ src/main/java/com/rudoy/hm018/Issue6/Neurosurgeon.java
@@ -1,16 +1,21 @@
 package com.rudoy.hm018.Issue6;
 
-/**
- * Created by OmEz on 20.05.2017.
- */
 public class Neurosurgeon implements Doctor {
+    private String name;
+    private int age;
+
+    public Neurosurgeon(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
     @Override
     public String getName() {
-        return "Artem";
+        return name;
     }
 
     @Override
     public int getAge() {
-        return 37;
+        return age;
     }
 }
--- src/main/java/com/rudoy/hm018/Issue6/Surgeon.java
+++ src/main/java/com/rudoy/hm018/Issue6/Surgeon.java
@@ -4,13 +4,22 @@ package com.rudoy.hm018.Issue6;
  * Created by OmEz on 20.05.2017.
  */
 public class Surgeon implements Doctor {
+
+    private String name;
+    private int age;
+
+    public Surgeon(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
     @Override
     public String getName() {
-        return "Alisa";
+        return name;
     }
 
     @Override
     public int getAge() {
-        return 25;
+        return age;
     }
 }
